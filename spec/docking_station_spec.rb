d99a532ce11f946bf9b3a17edffb3cfdc9b516fa
Joe Dowdell <joedowdell@gmail.com>
2014-07-10 10:42:39 +0100

    DockingStation provides list of available bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,6 @@
 require 'docking_station'
 
+
 describe DockingStation do
 	DEFAULT_CAPACITY = 10
 
@@ -23,14 +24,22 @@ describe DockingStation do
 	end
 
 	it "should know when it's full" do
-  expect(station).not_to be_full
-  fill_station station
-  expect(station).to be_full
-end
+	  expect(station).not_to be_full
+	  fill_station station
+	  expect(station).to be_full
+	end
 
 	it "should not accept a bike if it's full" do
 		fill_station station
 		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
+	end
+
+	it 'should provide a list of available bikes' do
+		working_bike, broken_bike = Bike.new, Bike.new
+		broken_bike.break!
+		station.dock(working_bike)
+		station.dock(broken_bike)
+		expect(station.available_bikes).to eq([working_bike])
 	end
 
 
